a7c156d68b6c37127a01b8773edefb0d8be7a676
An2 <an2m001@gmail.com>
2020-04-16 02:11:16 -0500

    startingTest

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
         </p>
         <a
               className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+              href="https://reactjs.org"
+           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
